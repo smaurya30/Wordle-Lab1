f0b2c58026ce6e839b20d94b57b7420046f714e9
smaurya30 <smaurya@stevens.edu>
2022-02-21 22:12:52 -0500

    Lab 5 Testing

---
+++ Lab5-Test/src/test/HW05_Shivani_Maurya_ui_test.py
@@ -0,0 +1,40 @@
+import unittest, sys
+from unittest.mock import patch, MagicMock
+magicMock = MagicMock()
+sys.modules['HW03_Shivani_Maurya_wordle'] = magicMock
+sys.modules['HW03_Shivani_Maurya_dictionary'] = magicMock
+from python.HW03_Shivani_Maurya_ui import checkWordle
+
+
+class Test(unittest.TestCase):
+    
+    @patch('builtins.input', side_effect=[[set(), 'organ', ['o','c','c','u','r'], 0,0,0,0,{'1':0, '2':0, '3':0, '4':0, '5':0, '6':0}]])
+    def testcheckWordle(self, mock_inputs):
+        res = checkWordle(set(), 'organ', ['o','c','c','u','r'], 1,0,1,0,{'1':0, '2':0, '3':0, '4':0, '5':0, '6':0})
+        self.assertEqual(res,({'organ'}, 1, 1, 2, 1, {'1': 1, '2': 0, '3': 0, '4': 0, '5': 0, '6':0}))
+        
+    @patch('builtins.input', side_effect=[[set(), 'lake', ['o','c','c','u','r'], 0,0,0,0,{'1':0, '2':0, '3':0, '4':0, '5':0, '6':0}]])
+    def testcheckWordleLength(self, mock_inputs):
+        res = checkWordle(set(), 'lake', ['o','c','c','u','r'], 1,0,1,0,{'1':0, '2':0, '3':0, '4':0, '5':0, '6':0})
+        self.assertEqual(res,(set(), 1, 0, 1, 0, {'1': 0, '2': 0, '3': 0, '4': 0, '5': 0, '6': 0}))
+        
+    
+    @patch('builtins.input', side_effect=[[set(), 'lake', ['o','c','c','u','r'], 0,0,0,0,{'1':0, '2':0, '3':0, '4':0, '5':0, '6':0}]])
+    def testcheckWordleSet(self, mock_inputs):
+        res = checkWordle({'aaron'}, 'aaron', ['o','c','c','u','r'], 1,0,1,0,{'1':0, '2':0, '3':0, '4':0, '5':0, '6':0})
+        self.assertEqual(res,({'aaron'}, 1, 0, 1, 0, {'1': 0, '2': 0, '3': 0, '4': 0, '5': 0, '6': 0}))
+        
+    @patch('builtins.input', side_effect=[[set(), 'lake', ['o','c','c','u','r'], 0,0,0,0,{'1':0, '2':0, '3':0, '4':0, '5':0, '6':0}]])
+    def testcheckWordleQuit(self, mock_inputs):
+        res = checkWordle({'aaron'}, '', ['o','c','c','u','r'], 1,0,1,0,{'1':0, '2':0, '3':0, '4':0, '5':0, '6':0})
+        self.assertEqual(res,({'aaron'}, 0, 0, 1, -1, {'1': 0, '2': 0, '3': 0, '4': 0, '5': 0, '6': 0}))
+        
+    @patch('builtins.input', side_effect=[[set(), 'lake', ['o','c','c','u','r'], 0,0,0,0,{'1':0, '2':0, '3':0, '4':0, '5':0, '6':0}]])
+    def testcheckWordleNotAlpha(self, mock_inputs):
+        res = checkWordle({'aaron'}, '@1ron', ['o','c','c','u','r'], 1,0,1,0,{'1':0, '2':0, '3':0, '4':0, '5':0, '6':0})
+        self.assertEqual(res,({'aaron'}, 1, 0, 1, 0, {'1': 0, '2': 0, '3': 0, '4': 0, '5': 0, '6': 0}))
+        
+        
+        
+if __name__ == '__main__':
+    unittest.main()
